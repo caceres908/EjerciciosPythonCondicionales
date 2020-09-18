fdbf9fd97e3f0122c86713fb19b307ca66fa921d
unknown <sacali99@gmail.com>
2020-09-17 20:08:29 -0500

    agrego pruebas unitarias del metodo palindromas

--- palabraCiclos.py
+++ palabraCiclos.py
@@ -1,3 +1,13 @@
 def palindroma(palabra):
-        if palabra == palabra[::-1]:
-            print("es palindroma")
+    inver = ""
+    for i in reversed(range(0, len(palabra))):
+        inver += palabra[i]
+    if palabra == inver:
+        pal = True
+    else:
+        pal = False
+    if pal:
+        print("Es palindroma")
+    else:
+        print("No es palindroma")
+    return pal
